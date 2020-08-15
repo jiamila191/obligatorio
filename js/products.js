7894109994b84e058690f7c3a4a5cc817779da95
Jiamila <jiamila.molina15@gmial.com>
2020-08-15 15:29:19 -0300

    Entrega 1

--- js/products.js
+++ js/products.js
@@ -4,3 +4,38 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+var productsArray = [];
+
+function showProductsList(array) {
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+        
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name + `</h4>
+                        <small class="font-muted">` + product.currency + " $" + product.cost + `<br> `+ product.soldCount + ` artículos vendidos </small>                    
+                     </div>
+                     ${product.description}
+                 </div>
+             </div>
+         </div>
+        `
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
+});
